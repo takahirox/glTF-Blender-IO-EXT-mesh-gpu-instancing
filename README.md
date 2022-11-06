5ae881c067310abff2f3dd70f21f1c579d6eeb4c
Takahiro <hogehoge@gachapin.jp>
2022-11-05 19:39:28 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # glTF-Blender-IO-EXT-mesh-gpu-instancing
+
+**glTF-Blender-IO-EXT-mesh-gpu-instancing** is a [Blender](https://www.blender.org/) addon for [glTF `EXT_mesh_gpu_instancing` extension](https://github.com/KhronosGroup/glTF/blob/main/extensions/2.0/Vendor/EXT_mesh_gpu_instancing/README.md) on top of [`glTF-Blender-IO`](https://github.com/KhronosGroup/glTF-Blender-IO) addon.
+
+## Compatible Blender version
+
+&gt;= 3.3
+
+## Features
+
+The addon enables to export glTF assets with the [glTF `EXT_mesh_gpu_instancing` extension](https://github.com/KhronosGroup/glTF/blob/main/extensions/2.0/Vendor/EXT_mesh_gpu_instancing/README.md) in Blender.
+
+Import support is work in progress.
+
+## How to install
+
+### Ensure the `glTF-Blender-IO` addon is installed and enabled
+
+Ensure the `glTF-Blender-IO` addon is installed and enabled in your Blender because the `glTF-Blender-IO-EXT-mesh-gpu-instancing
+` addon works on top of it.  You can check it by Edit -> Preferences -> Add-ons -> "glTF 2.0" in the search bar. The `glTF-Blender-IO` addon should be listed as `Import-Export: glTF 2.0 format`. It should be installed and enabled by default.
+
+![Ensure glTF-Blender-IO is installed and enabled](https://user-images.githubusercontent.com/7637832/110406787-a41f3f80-8037-11eb-9e12-163aafd5f08e.png)
+
+### Download the zip archived source code
+
+Download the zip archived source code from the [Releases](https://github.com/takahirox/glTF-Blender-IO-EXT-mesh-gpu-instancing/releases).
+
+### Install `glTF-Blender-IO-EXT-mesh-gpu-instancing` addon
+
+Install the `glTF-Blender-IO-EXT-mesh-gpu-instancing` addon to your Blender via Edit -> Preferences -> Add-ons -> Install -> Select the downloaded file
+
+![Edit -> Preferences](https://user-images.githubusercontent.com/7637832/110405180-062a7580-8035-11eb-839a-f5008a992f92.png)
+
+![Add-ons -> Install](https://user-images.githubusercontent.com/7637832/110405413-70dbb100-8035-11eb-9860-3f4867427246.png)
+
+![Select the downloaded file](https://user-images.githubusercontent.com/7637832/200152177-afacdc7d-5349-435e-bc80-7951c071b1b8.png)
+
+### Enable the addon
+
+Ensure the addon is installed and enabled. You can easily find the addon by inputting "EXT_mesh_gpu_instancing" in the search bar. Check the checkbox to enable the addon.
+
+![Ensure the addon is enabled](https://user-images.githubusercontent.com/7637832/200152215-39be04be-3b6d-4a34-9704-c5f42d61dbf6.png)
+
+## How to use
+
+T.B.D.
+
+**Blender + Geometry Nodes + Instancing**
+
+<img src="https://user-images.githubusercontent.com/7637832/200151844-c4ade71d-0507-4268-8356-139157fb2db6.png" width="640" alt="Blender + Geometry Nodes + Instancing">
+
+**Exported glTF**
+
+<img src="https://user-images.githubusercontent.com/7637832/200151884-2f335fd0-3270-4b68-abc1-998837a0f9c6.png" width="480" alt="Exported glTF">
---
+++ __init__.py
@@ -0,0 +1,129 @@
+import bpy
+from io_scene_gltf2.io.com import gltf2_io_constants
+from io_scene_gltf2.blender.exp import gltf2_blender_export_keys
+from io_scene_gltf2.blender.exp import gltf2_blender_gather_nodes
+from io_scene_gltf2.blender.exp import gltf2_blender_gather_mesh
+from io_scene_gltf2.blender.exp import gltf2_blender_gather_primitive_attributes
+from io_scene_gltf2.blender.imp.gltf2_blender_node import BlenderNode
+
+bl_info = {
+    "name" : "glTF EXT_mesh_gpu_instancing IO",
+    "author" : "Takahiro Aoyagi",
+    "description" : "Addon for glTF EXT_mesh_gpu_instancing extension",
+    "blender" : (3, 3, 0),
+    "version" : (0, 0, 1),
+    "location" : "",
+    "wiki_url": "https://github.com/takahirox/glTF-Blender-IO-EXT-mesh-gpu-instancing/wiki",
+    "tracker_url": "https://github.com/takahirox/glTF-Blender-IO-EXT-mesh-gpu-instancing/issues",
+    "support": "COMMUNITY",
+    "warning" : "",
+    "category" : "Generic"
+}
+
+glTF_extension_name = "EXT_mesh_gpu_instancing"
+
+def register():
+    return
+
+def unregister():
+    return
+
+# Export
+
+class glTF2ExportUserExtension:
+    def __init__(self):
+        from io_scene_gltf2.io.com.gltf2_io_extensions import Extension
+        self.Extension = Extension
+
+    # @TODO: Optimize
+    def gather_node_hook(self, gltf2_node, blender_object, export_settings):
+        if not export_settings[gltf2_blender_export_keys.APPLY]:
+            return
+
+        depsgraph = bpy.context.evaluated_depsgraph_get()
+        blender_object_eval = blender_object.evaluated_get(depsgraph)
+
+        found = False
+        locations = []
+        rotations = []
+        scales = []
+        for object_instance in depsgraph.object_instances:
+            if blender_object_eval != object_instance.instance_object:
+                continue
+            found = True
+
+            blender_mesh = object_instance.object.data
+            materials = tuple(ms.material for ms in object_instance.object.material_slots)
+            result = gltf2_blender_gather_mesh.gather_mesh(
+                blender_mesh,
+                None, # uuid_for_skined_data,
+                None, # vertex_groups,
+                None, # modifiers,
+                True, # skip_filter
+                materials,
+                None, # original_mesh
+                export_settings
+            )
+
+            gltf2_node.mesh = result
+
+            # @TODO: Convert world space matrix to parent (this node)
+            #        space matrix
+            loc, rot, sca = object_instance.matrix_world.decompose()
+
+            if export_settings[gltf2_blender_export_keys.YUP]:
+                locations.append(loc.x)
+                locations.append(loc.z)
+                locations.append(-loc.y)
+
+                rotations.append(rot.x)
+                rotations.append(rot.z)
+                rotations.append(-rot.y)
+                rotations.append(rot.w)
+
+                scales.append(sca.x)
+                scales.append(sca.z)
+                scales.append(sca.y)
+            else:
+                locations.append(loc.x)
+                locations.append(loc.y)
+                locations.append(loc.z)
+
+                rotations.append(rot.x)
+                rotations.append(rot.y)
+                rotations.append(rot.z)
+                rotations.append(rot.w)
+
+                scales.append(sca.x)
+                scales.append(sca.y)
+                scales.append(sca.z)
+
+        if not found:
+            return
+
+        if gltf2_node.extensions is None:
+            gltf2_node.extensions = {}
+        gltf2_node.extensions[glTF_extension_name] = self.Extension(
+            name=glTF_extension_name,
+            extension={"attributes": {
+                "TRANSLATION": gltf2_blender_gather_primitive_attributes.array_to_accessor(
+                    locations,
+                    component_type=gltf2_io_constants.ComponentType.Float,
+                    data_type=gltf2_io_constants.DataType.Vec3,
+                    include_max_and_min=True,
+                ),
+                "ROTATION": gltf2_blender_gather_primitive_attributes.array_to_accessor(
+                    rotations,
+                    component_type=gltf2_io_constants.ComponentType.Float,
+                    data_type=gltf2_io_constants.DataType.Vec4,
+                    include_max_and_min=True,
+                ),
+                "SCALE": gltf2_blender_gather_primitive_attributes.array_to_accessor(
+                    scales,
+                    component_type=gltf2_io_constants.ComponentType.Float,
+                    data_type=gltf2_io_constants.DataType.Vec3,
+                    include_max_and_min=True,
+                ),
+            }},
+            required=False
+        )
