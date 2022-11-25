ae83f190c5354f6861e81fc7629624fdc7d59f3f
Takahiro <hogehoge@gachapin.jp>
2022-11-24 19:51:28 -0800

    Fix local matrix convert

--- __init__.py
+++ __init__.py
@@ -44,13 +44,19 @@ class glTF2ExportUserExtension:
         blender_object_eval = blender_object.evaluated_get(depsgraph)
 
         found = False
+        parent_world_matrix_inverted = None
+
         locations = []
         rotations = []
         scales = []
+
         for object_instance in depsgraph.object_instances:
             if not object_instance.is_instance or blender_object_eval != object_instance.parent:
                 continue
-            found = True
+
+            if not found:
+                found = True
+                parent_world_matrix_inverted = object_instance.parent.matrix_world.inverted_safe()
 
             blender_mesh = object_instance.object.data
             materials = tuple(ms.material for ms in object_instance.object.material_slots)
@@ -67,7 +73,8 @@ class glTF2ExportUserExtension:
 
             gltf2_node.mesh = result
 
-            loc, rot, sca = object_instance.object.matrix_local.decompose()
+            local_matrix = parent_world_matrix_inverted @ object_instance.matrix_world
+            loc, rot, sca = local_matrix.decompose()
 
             if export_settings[gltf2_blender_export_keys.YUP]:
                 locations.append(loc.x)
